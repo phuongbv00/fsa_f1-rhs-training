c513e2cad3361cec8a0e2c36fd34c5bde54d2d38
Phuong Bui <buiphuongaptutc@gmail.com>
2025-06-03 13:47:55 +0700

    init

---
+++ src/main/java/fsa/f1rhstraining/F1RhsTrainingApplication.java
@@ -0,0 +1,13 @@
+package fsa.f1rhstraining;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class F1RhsTrainingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(F1RhsTrainingApplication.class, args);
+    }
+
+}
