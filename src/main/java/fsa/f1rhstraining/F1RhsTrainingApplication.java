2c5eabf4f80a94b15ee4eb7abf1cd3dc142688be
Phuong Bui <buiphuongaptutc@gmail.com>
2025-06-16 15:56:26 +0700

    feat: batch p1

--- src/main/java/fsa/f1rhstraining/F1RhsTrainingApplication.java
+++ src/main/java/fsa/f1rhstraining/F1RhsTrainingApplication.java
@@ -1,13 +1,30 @@
 package fsa.f1rhstraining;
 
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import java.time.Instant;
 
 @SpringBootApplication
 public class F1RhsTrainingApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(F1RhsTrainingApplication.class, args);
+        ApplicationContext context = SpringApplication.run(F1RhsTrainingApplication.class, args);
+        JobLauncher jobLauncher = context.getBean(JobLauncher.class);
+        Job postDelJob = context.getBean("postDelJob", Job.class);
+        try {
+            JobParameters params = new JobParametersBuilder()
+                    .addString("startTime", Instant.now().toString())
+                    .toJobParameters();
+            jobLauncher.run(postDelJob, params);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
 }
